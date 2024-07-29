ce9aa0369d01e2d6fcfa6e677942273a0c74880e
wftb <145103446+wftb@users.noreply.github.com>
2024-07-30 07:11:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'dee64176-3d81-4667-a814-44b14db7f13c';
 
 // const พร็อกซีไอพีs = ['proxyip.oracle.fxxk.dedyn.io'];
 
