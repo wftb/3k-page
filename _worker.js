f3c61267d7257a8b4db17c370eb813a6f97d7579
wftb <145103446+wftb@users.noreply.github.com>
2024-09-21 21:17:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ let userID = '05153d24-5d01-421b-9632-994a4835cb22';
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 // let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
 // use single พร็อกซีไอพี instead of random
-let พร็อกซีไอพี = 'proxyip.oracle.fxxk.dedyn.io';
+let พร็อกซีไอพี = 'ProxyIP.US.fxxk.dedyn.io';
 // ipv6 พร็อกซีไอพี example remove comment to use
 // let พร็อกซีไอพี = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
 
